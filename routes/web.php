9b33392568943788a2b81285690eb45f6a9ada62
claudiopatti <claudiopatti1997@gmail.com>
2024-10-15 12:56:00 +0200

    aggiunta pagina con chi siamo

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/chi-siamo', function () {
+    return view('chi-siamo');
 });
